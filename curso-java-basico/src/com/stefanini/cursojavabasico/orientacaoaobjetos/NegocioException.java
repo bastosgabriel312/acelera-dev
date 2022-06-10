ad78ee52ee0422845d06c305f19c6cf646d84cf6
bastosgabriel312 <bastosgabriel312@gmail.com>
2022-06-09 23:15:29 -0300

    Aula 15 - Exceptions

---
+++ curso-java-basico/src/com/stefanini/cursojavabasico/orientacaoaobjetos/NegocioException.java
@@ -0,0 +1,10 @@
+package com.stefanini.cursojavabasico.orientacaoaobjetos;
+
+public class NegocioException extends Exception {
+
+	public NegocioException(String message) {
+		super(message);
+	}
+	
+
+}
--- curso-java-basico/src/com/stefanini/cursojavabasico/orientacaoaobjetos/Principal.java
+++ curso-java-basico/src/com/stefanini/cursojavabasico/orientacaoaobjetos/Principal.java
@@ -7,30 +7,38 @@ public class Principal {
 		bmw.setMarca("BMW");
 		bmw.setModelo("M3");
 		bmw.setQuilometrosPorLitro(7.5);
-		bmw.setVelocidadeMaxima(200);
-		
+
+		try {
+			bmw.setVelocidadeMaxima(10);
+		} catch (NegocioException e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("Passou aqui");
+		}
+
 		BMW bmw2 = new BMW();
-		Carro mercedes = new Carro("Mercedes","c180");
-		
+		Carro mercedes = new Carro("Mercedes", "c180");
+		BMW bmw3 = null;
 		Tesla tesla = new Tesla();
 		Carro tesla2 = new Tesla();
 		System.out.println(tesla.isDirigeSozinho());
-		
-		//		cast
+
+		// cast
 		((Tesla) tesla2).setDirigeSozinho(true);
 		System.out.println(((Tesla) tesla2).isDirigeSozinho());
-		
-		Carro[] carros = new Carro[] {bmw, bmw2, tesla,tesla2};
-		
+
+		Carro[] carros = new Carro[] { bmw, bmw2, tesla, tesla2 };
+
 		for (Carro carro : carros) {
 			if (carro instanceof Tesla) {
 				System.out.println(((Tesla) carro).isDirigeSozinho());
 			}
 		}
-		
+
 		System.out.println(bmw2.calcularTaxaAceleracao(50, 2));
 		System.out.println(tesla.calcularTaxaAceleracao(50, 2));
-		
+//		bmw3.acelerar();
+
 //		System.out.println(bmw2.calcularTaxaAceleracao(100, 10));
 //		System.out.println(mercedes.getMarca());
 //		System.out.println(mercedes.getModelo());
