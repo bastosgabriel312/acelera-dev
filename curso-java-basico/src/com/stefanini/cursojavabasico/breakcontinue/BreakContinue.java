28a1eb79aab612d940e57cda1368aa30330be442
bastosgabriel312 <bastosgabriel312@gmail.com>
2022-06-09 22:55:01 -0300

    Aula 14 - Break Continue

---
+++ curso-java-basico/src/com/stefanini/cursojavabasico/breakcontinue/BreakContinue.java
@@ -0,0 +1,28 @@
+package com.stefanini.cursojavabasico.breakcontinue;
+
+public class BreakContinue {
+
+	public static void main(String[] args) {
+		String[] carros = new String[] { "BMW i320", "Mercedes c180", "BMW M3", "Mercedes AMG", "Tesla Model S" };
+		
+		for (int i=0; i < carros.length; i++) {
+			if(carros[i]=="BMW i320") {
+				System.out.println(carros[i]);
+				break;
+			}
+			System.out.println(carros[i]);
+		}
+
+		for (int i=0; i < carros.length; i++) {
+			if(carros[i]=="BMW i320") {
+				System.out.println(carros[i]);
+				continue;
+			}
+			System.out.println("Quero esse carro");
+			System.out.println(carros[i]);
+		}
+
+		
+	}
+
+}
